34e88e2dd968267e80691866149e19be53dbf370
cherifa.informatique@gmail.com <cherifa.informatique@gmail.com>
2024-05-28 10:38:45 +0100

    updated

--- src/main/java/com/example/santevistabackendpfe/controllers/AuthenticationController.java
+++ src/main/java/com/example/santevistabackendpfe/controllers/AuthenticationController.java
@@ -7,10 +7,7 @@ import com.example.santevistabackendpfe.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +15,7 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/public/authentication-resources")
+@CrossOrigin
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 //    @PostMapping("/login")
